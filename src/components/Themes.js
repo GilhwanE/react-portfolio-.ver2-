7f091bc1f779f128caa61b1a29aa0aa0417808bf
GilhwanE <dyghks123@gmail.com>
2022-05-17 19:50:45 +0900

    💄 Add Themes (dark , light )

---
+++ src/components/Themes.js
@@ -0,0 +1,17 @@
+// Theme option : dark-mode and light-mode
+
+export const lightTheme = {
+  body: '#FCF6F4',
+  text: '#000000',
+  fontFamily: "'Lato', sans-serif",
+  bodyRgba: '252, 246, 244',
+  textRgba: '0,0,0',
+};
+
+export const darkTheme = {
+  body: '#000000',
+  text: '#FCF6F4',
+  fontFamily: "'Lato', sans-serif",
+  bodyRgba: '252, 246, 244',
+  textRgba: '0,0,0',
+};
