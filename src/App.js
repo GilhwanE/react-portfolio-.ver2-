56e2e37ea4dd1f015d81a6e2f6a5c8667e0b110d
GilhwanE <dyghks123@gmail.com>
2022-05-23 14:52:17 +0900

    🚧  Create components and router-set

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
+import { lightTheme } from './components/Themes';
+import GlobalStyle from './globalStyle';
+
+// components
+import About from './components/About';
+import Main from './components/Main';
+import MySkils from './components/MySkils';
+import Work from './components/Work';
 
 function App() {
-  return 'ggg';
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/MySkils" element={<MySkils />} />
+            <Route path="/Work" element={<Work />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
