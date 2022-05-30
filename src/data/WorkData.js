33eaef6bbf85df06b8f38ea46c4022027ddc3333
GilhwanE <dyghks123@gmail.com>
2022-05-30 16:35:52 +0900

    📝  Create WorkData Files

---
+++ src/data/WorkData.js
@@ -0,0 +1,53 @@
+export const WorkData = [
+  {
+    id: 1,
+    name: 'Portfolio Page',
+    description:
+      'HTML, CSS, JS를 이용하여 만든 자기소개 포트롤리오 반응형 웹페이지.',
+    tags: ['HTML', 'CSS', 'VanilJS'],
+
+    demo: 'https://GilhwanE.github.io/portfolio/',
+    github: 'https://github.com/GilhwanE/portfolio',
+  },
+
+  {
+    id: 2,
+    name: 'Tommrow House Page',
+    description: '오늘의집을 클론코딩한 웹페이지이며, Sass를 이용하여 제작.',
+    tags: ['HTML', 'CSS', 'VanilJS', 'Sass'],
+
+    demo: 'https://gilhwane.github.io/house_t_webpage/',
+    github: 'https://github.com/GilhwanE/house_t_webpage',
+  },
+
+  {
+    id: 3,
+    name: 'Emotion-Diary with React',
+    description: 'React를 사용하여 CRUD의 기능을 중심으로 제작한 일기장 앱',
+    tags: ['React', 'Firebase'],
+
+    demo: 'https://hwan-react-project-diary.web.app/',
+    github: 'https://github.com/GilhwanE/emotion-diary',
+  },
+
+  {
+    id: 4,
+    name: 'Portfolio React Page',
+    description: 'React를 이용하여 만든 자기소개 포트롤리오 반응형 웹페이지.',
+    tags: ['React', 'style-component'],
+    img: '../assets/imgaes/CSS-icon.png',
+
+    demo: 'https://GilhwanE.github.io/portfolio/',
+    github: 'https://github.com/GilhwanE/react-portfolio-.ver2-',
+  },
+
+  {
+    id: 5,
+    name: 'Do it Plan',
+    description: 'TypeScript, React 이용하여 만든 TO-DO-LIST.',
+    tags: ['React', 'TypeScript', 'netlify'],
+
+    demo: 'https://tranquil-alfajores-ebe2ce.netlify.app',
+    github: 'https://github.com/GilhwanE/doit-plan',
+  },
+];
