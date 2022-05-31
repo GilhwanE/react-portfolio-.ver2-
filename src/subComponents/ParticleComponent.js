2ed24b49bf8df6c7e87a6b40ef134cfb9add53f1
GilhwanE <dyghks123@gmail.com>
2022-05-31 14:48:44 +0900

    ✨  Particle-animation

--- src/subComponents/ParticleComponent.js
+++ src/subComponents/ParticleComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 // config files
-import Particles from 'react-tsparticles';
+import ParticleAnimation from 'react-particle-animation';
 
 const Box = styled.div`
   position: absolute;
@@ -15,83 +15,14 @@ const Box = styled.div`
 const ParticleComponent = (props) => {
   return (
     <Box>
-      <Particles
-        options={{
-          background: {
-            color: {
-              value: '#0d47a1',
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: 'push',
-              },
-              onHover: {
-                enable: true,
-                mode: 'repulse',
-              },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: '#ffffff',
-            },
-            links: {
-              color: '#ffffff',
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: 'none',
-              enable: true,
-              outMode: 'bounce',
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: 'circle',
-            },
-            size: {
-              random: true,
-              value: 5,
-            },
-          },
-          detectRetina: true,
+      <ParticleAnimation
+        numParticles={300}
+        interactive={false}
+        background={{ r: 100, g: 100, b: 100, a: 255 }}
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
         }}
       />
     </Box>
